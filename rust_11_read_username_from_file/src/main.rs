90803baced3fd67b6776bdbb575bd18f78f70157
Feather-P <featherp@qq.com>
2025-07-26 11:58:49 +0800

    Add: 11-使用result和fs从文件读取用户名

---
+++ rust_11_read_username_from_file/src/main.rs
@@ -0,0 +1,29 @@
+use std::{fs,fs::File, io::{self, Read}, result, string};
+
+fn main() {
+    fn read_username_from_file(file_name: &str) -> Result<String, io::Error> {
+        let file_result = File::open(file_name);
+        let mut user_name_file = match file_result{
+            Ok(file) => file,
+            Err(e   ) => return Err(e)
+        };
+
+        let mut user_name = String::new();
+
+        match user_name_file.read_to_string(&mut user_name){
+            Ok(_) => Ok(user_name),
+            Err(e) => Err(e),
+        }
+    }
+
+    fn read_username_from_file_easy1(file_name: &str) -> Result<String, io::Error> {
+        let mut user_name = String::new();
+        File::open(file_name)?.read_to_string(&mut user_name)?;
+        Ok(user_name)
+    }
+
+    fn read_username_from_file_easy2(file_name: &str) -> Result<String, io::Error> {
+        fs::read_to_string(&file_name)
+        // The Easiest way
+    }
+}
