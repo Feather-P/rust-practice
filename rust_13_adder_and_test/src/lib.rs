8d6946c1cbd56308d6a283d1e0f5feaac5c37deb
Feather-P <featherp@qq.com>
2025-07-27 11:22:55 +0800

    Add: 13-初识单元测试与集合测试

---
+++ rust_13_adder_and_test/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[should_panic(expected = "test fail")]
+    fn fault(){
+        panic!("Make this test fail")
+    }
+}
---
+++ rust_13_adder_and_test/tests/integrated_test.rs
@@ -0,0 +1,6 @@
+use rust_13_adder_and_test;
+
+#[test]
+fn add_test(){
+    assert_eq!(rust_13_adder_and_test::add(2, 5), 7)
+}
